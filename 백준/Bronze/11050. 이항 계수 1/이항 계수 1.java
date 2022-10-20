ad739a16135f45dc392ce6c4a376d0f54a709920
이정인 <dlwjddls0130@naver.com>
2022-10-20 19:10:47 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    // 반복문으로 구현 : 재귀는 나올때도 곱하면서 나와서 너무 비효율적
     public static int factorial(int n) {
         int fac = 1;
         for (int i = 2; i <= n; i++) {

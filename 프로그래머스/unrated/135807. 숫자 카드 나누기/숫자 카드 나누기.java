bb9a780ea6008cce31323fed27cbe6857c315697
이정인 <dlwjddls0130@naver.com>
2023-01-07 19:17:02 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+
+        // 하나의 최대공약수가 1이고, 하나는 최대가 되어야함.
+        // 모두를 나누는 최대공약수도 1이여야함.
+        int a = arrayA[0];
+        for (int i = 1; i < arrayA.length; i++) {
+            if (a > arrayA[i]) {
+                a = GCD(a, arrayA[i]);
+            } else {
+                a = GCD(arrayA[i], a);
+            }
+
+        }
+
+        int b = arrayB[0];
+        for (int i = 1; i < arrayB.length; i++) {
+            if (b > arrayB[i]) {
+                b = GCD(b, arrayB[i]);
+            } else {
+                b = GCD(arrayB[i], b);
+            }
+        }
+
+        if (a > b) {
+            answer = a;
+
+            for (int i = 0; i < arrayB.length; i++) {
+                if (arrayB[i] % a == 0) {
+                    return 0;
+                }
+            }
+        }
+
+        if (a < b) {
+            answer = b;
+
+            for (int i = 0; i < arrayA.length; i++) {
+                if (arrayA[i] % b == 0) {
+                    return 0;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    private int GCD(int A, int B) {
+        if (B == 0) {
+            return A;
+        }
+        return GCD(B, A % B);
+    }
+}

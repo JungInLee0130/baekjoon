09d7d37e0ba6a00245e420f736a932721e9a8ef3
이정인 <dlwjddls0130@naver.com>
2023-01-08 23:05:29 +0900

    [Silver II] Title: N과 M (12), Time: 288 ms, Memory: 25472 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 사전순이라 sort먼저
+        Arrays.sort(array);
+
+        // N개중 M개를 뽑는 경우를 모두 출력
+        visited = new boolean[N]; // N개 // 방문기록 체크
+        answer = new int[M]; // 순열을 담을 배열
+        entireHashSet = new LinkedHashSet<>(); // LinkedHashSet -> 입력순으로 정렬
+        Permutation(0, 0);
+
+        entireHashSet.forEach(System.out::println);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    private static int N;
+    private static int M;
+    private static boolean[] visited;
+    private static int[] array;
+    private static HashSet<String> entireHashSet;
+    private static int[] answer;
+    private static void Permutation(int depth, int start) {
+        if (depth == M) { // N개의 조합이 완성
+            StringBuilder sb = new StringBuilder();
+            for (int element: answer) {
+                sb.append(element).append(' '); // element + ' '
+            }
+            entireHashSet.add(sb.toString());
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            /*if (!visited[i]) {
+                visited[i] = true;*/
+                answer[depth] = array[i];
+                Permutation(depth + 1, i);
+                /*visited[i] = false;
+            }*/
+        }
+    }
+}

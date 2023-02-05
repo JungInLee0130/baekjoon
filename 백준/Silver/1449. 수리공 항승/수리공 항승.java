ef7dba70b5a5616fbdec4a5bbf48a150abce483d
이정인 <dlwjddls0130@naver.com>
2023-02-05 15:50:41 +0900

    [Silver III] Title: 수리공 항승, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -18,6 +18,7 @@ public class Main {
             leak[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 정렬
         Arrays.sort(leak);
 
         if (N == 1) bw.write(String.valueOf(1));
@@ -25,20 +26,27 @@ public class Main {
             int count = 0;
             int sub = 0;
             for (int i = 0; i < N; i++) {
+                // 결국 끝점에 도달해서 테이프를 한개 밖에 붙일수밖에 없는 상황
+                // 붙일 수 있었으면 이전점에서 끝났을꺼임.
                 if (i == N - 1){
                     count += 1;
                     break;
                 }
+                // 차를 구하기 : 양쪽 0.5만큼의 차가 필요하므로 +1
                  sub += leak[i + 1] - leak[i] + 1;
 
-                if (sub > L) { // L보다 크면
+                // 차가 L보다 크면 : 테이프 1개로 커버해야하는 상황
+                if (sub > L) {
                     count++;
                     sub = 0;
+                    // 테이프 길이가 같으면 : 적어도 2개이상 커버가능
+                    // 시작지점부터 끝지점까지 커버가 가능하기때문에 끝지점 다음점부터 시작
                 } else if (sub == L) {
                     count++;
                     i = i + 1;
                     sub = 0;
-                } else { // L보다 작으면
+                } else {
+                    // 테이프 길이가 남는다는 뜻이므로 다음 점끼리 차를 더해준다. +1했던것은 뺌.
                     // do nothing
                     sub -= 1;
                 }

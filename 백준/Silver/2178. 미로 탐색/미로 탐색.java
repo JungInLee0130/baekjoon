9e08800cd31a70e042e5bb2a637e58a7e3381e8a
이정인 <dlwjddls0130@naver.com>
2023-02-10 16:31:40 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,101 +1,72 @@
-import java.awt.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static boolean[][] visited;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int row = Integer.parseInt(st.nextToken());
-        int column = Integer.parseInt(st.nextToken());
-
-        int[][] maze = new int[row][column];
-
-        for (int i = 0; i < row; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < column; j++) {
-                maze[i][j] = str.charAt(j) - '0';
-            }
-        }
-
-        visited = new boolean[row][column];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                visited[i][j] = false;
-            }
-        }
-
-        mazeRunner(maze, row, column);
-
-        bw.write(String.valueOf(minWay));
-
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-
-    private static void mazeRunner(int[][] maze, int row, int column) {
-         BFS(maze, 0, 0, row, column);
-    }
-
-    private static Queue<Point> queue = new LinkedList<>();
-    private static int minWay = 0;
-    private static void BFS(int[][] maze, int i, int j, int row, int column) {
-        visited[i][j] = true;
-        queue.add(new Point(i, j));
-        int queueSize = 1;
-        int childNum = 1;
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            queueSize--;
-            if (queueSize == 0) {
-                queueSize = childNum;
-                childNum = 0;
-                minWay++;
-            }
-            int x = p.x;
-            int y = p.y;
-            if (x >= 1) {
-                if (maze[x - 1][y] == 1 && !visited[x - 1][y]) {
-                    visited[x - 1][y] = true;
-                    childNum++;
-                    queue.add(new Point(x - 1, y));
-                }
-            }
-            if (x < row - 1) {
-                if (maze[x + 1][y] == 1 && !visited[x + 1][y]) {
-                    visited[x + 1][y] = true;
-                    childNum++;
-                    queue.add(new Point(x + 1, y));
-                }
-            }
-
-            if (y >= 1) {
-                if (maze[x][y - 1] == 1 && !visited[x][y - 1]) {
-                    visited[x][y - 1] = true;
-                    childNum++;
-                    queue.add(new Point(x, y - 1));
-                }
-            }
-
-            if (y < column - 1) {
-                if (maze[x][y + 1] == 1 && !visited[x][y + 1]) {
-                    visited[x][y + 1] = true;
-                    childNum++;
-                    queue.add(new Point(x, y + 1));
-                }
-            }
-            if (p.x == row - 1 && p.y == column - 1) {
-                return;
-            }
-        }
-    }
+	static int N,M;
+	static int[][] map;
+	static int[][] visited;
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1}; // 상 우 하 좌
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		visited = new int[N][M];
+		
+		
+		// 1 : 이동 가능 0 : 이동 x
+		// 4방, 8방 검사 -> 4방
+		// 1,1 -> N, M 최소칸수 -> BFS
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine(); //
+			char[] cs = s.toCharArray();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = cs[j] - '0'; // '1' - '0' = 49 - 48
+			}
+		}
+		
+		bfs();
+		System.out.println(visited[N - 1][M - 1]);
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	private static void bfs() {
+		Queue<int[]> que = new LinkedList<>();
+		que.offer(new int[] {0,0});
+		visited[0][0] = 1;
+		while (!que.isEmpty()) {
+			int[] cur = que.poll();
+			int r = cur[0];
+			int c = cur[1];
+			
+			if (r == N - 1 && c == M - 1) {
+				return;
+			}
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				if (!check(nr, nc)) continue;
+				if (map[nr][nc] == 1 && visited[nr][nc] == 0) {
+					que.offer(new int[] {nr,nc});
+					visited[nr][nc] = visited[r][c] + 1;
+				}
+			}
+		}
+	}
+	private static boolean check(int nr, int nc) {
+		return 0 <= nr && nr < N && 0 <= nc && nc < M;
+	}
 }

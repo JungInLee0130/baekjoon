15e6355593751c254ac7b8513ce566bb83aa2c6d
이정인 <dlwjddls0130@naver.com>
2023-02-13 16:18:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 84 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,94 +1,86 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static boolean[][] visited;
-    private static int houseNumber = 0;
-    private static ArrayList<Integer> houseSpecificNumber = new ArrayList<>();
-    private static int specificNumber = 0;
-    private static void DFS(int[][] house, int x, int y) {
-        visited[x][y] = true;
-        specificNumber++;
+	static int[][] map;
+	static int N;
+	static int[] count;
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1}; // 상우하좌
+	static int cnt;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		count = new int[N * N];
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			char[] sc = str.toCharArray();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = sc[j] - '0';
+			}
+		}
+		cnt = 0; // group
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				// 모든 지점에서 1인곳을 찾아서 상하좌우로 연결시킨다.
+				if (map[i][j] == 1) {
+					cnt++;
+					count[cnt + 1] = 1; // 2~
+					dfs(i,j, cnt + 1);
+				}
+			}
+		}
+		/*for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}*/
+		System.out.println(cnt);
+		ArrayList<Integer> result = new ArrayList<>();
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] > 0) {
+				result.add(count[i]);
+			}
+		}
+		Collections.sort(result);
+		for (Integer integer : result) {
+			bw.write(String.valueOf(integer) + "\n");
+		}
+		
+		
+		//System.out.println(cnt);
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	// g으로 1로 상하좌우로 연결된것을 g로 만든다.
+	private static void dfs(int r, int c, int g) {
+		map[r][c] = g;
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if (!check(nr,nc)) continue;
+			if (map[nr][nc] == 1) {
+				count[g]++;
+				dfs(nr, nc, g);
+			}
+		}
+	}
+	private static boolean check(int nr, int nc) {
+		return 0 <= nr && nr < N && 0 <= nc && nc < N;
+	}
 
-        if (x >= 1) {
-            if (house[x - 1][y] == 1 && !visited[x - 1][y]) {
-                DFS(house, x - 1, y);
-            }
-        }
-
-        if (y >= 1) {
-            if (house[x][y - 1] == 1 && !visited[x][y - 1]) {
-                DFS(house, x, y - 1);
-            }
-        }
-
-        if (x <= N - 2) {
-            if (house[x + 1][y] == 1 && !visited[x + 1][y]) {
-                DFS(house, x + 1, y);
-            }
-        }
-
-        if (y <= N - 2) {
-            if (house[x][y + 1] == 1 && !visited[x][y + 1]) {
-                DFS(house, x, y + 1);
-            }
-        }
-    }
-
-    private static void naming(int[][] house) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (house[i][j] == 0 || visited[i][j]) { // 0인곳을 따라 DFS
-                    continue;
-                } else {
-                    DFS(house, i, j);
-                    houseNumber++;
-                    houseSpecificNumber.add(specificNumber);
-                    specificNumber = 0;
-                }
-            }
-        }
-
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-
-        int[][] house = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                house[i][j] = str.charAt(j) - '0';
-            }
-        }
-
-        visited = new boolean[N][N];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                visited[i][j] = false;
-            }
-        }
-
-        naming(house);
-
-        bw.write(String.valueOf(houseNumber) + "\n");
-
-        Collections.sort(houseSpecificNumber);
-
-        for (Integer num: houseSpecificNumber) {
-            bw.write(String.valueOf(num) + "\n");
-        }
-        bw.flush();
-        br.close();
-        bw.close();
-    }
 }

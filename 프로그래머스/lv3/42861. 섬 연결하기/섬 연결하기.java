d87f8bde25e1a5aaeb447d2a969f73449aa27205
이정인 <dlwjddls0130@naver.com>
2023-02-15 09:22:42 +0900

    [level 3] Title: 섬 연결하기, Time: 0.50 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 
 class Solution {
+    static int total;
+    static ArrayList<ArrayList<Vertex>> graph;
+    
     static class Vertex implements Comparable<Vertex>{
         int num;
         int weight;
@@ -12,90 +15,51 @@ class Solution {
         }
 
         @Override
-        public int compareTo(Vertex v) {
-            return this.weight - v.weight;
+        public int compareTo(Vertex o) {
+            return this.weight - o.weight;
         }
     }
-    private static ArrayList<ArrayList<Vertex>> graph;
-    private static int max;
-    private static boolean[] visited;
+   
     public int solution(int n, int[][] costs) {
-        max = 0;
-        // costs[N][0] : 시작점
-        // costs[N][1] : 끝점
-        // costs[N][2] : 가중치
+          graph = new ArrayList<>();
 
-        // 그래프 연결
-        graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < n; i++) { // n : 간선의 개수
-            int start = costs[i][0];
-            int end = costs[i][1];
-            int weight = costs[i][2];
+        for (int[] edge: costs) {
+            int from = edge[0];
+            int to = edge[1];
+            int cost = edge[2];
 
-            graph.get(start).add(new Vertex(end, weight));
-            graph.get(end).add(new Vertex(start, weight));
+            graph.get(from).add(new Vertex(to, cost));
+            graph.get(to).add(new Vertex(from, cost));
         }
 
-
-        for (int i = 0; i < n; i++) {
-            visited = new boolean[n];
-
-            max = Math.max(dijkstra(n, i), max);
-        }
-
-        return max;
+        prim(0, n);
+        return total;
     }
+    private void prim(int start, int n) {
+        boolean[] visited = new boolean[n];
 
-    private static int[] minWeight;
-    private int dijkstra(int n, int start) {
-        PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
-        minWeight = new int[n];
-        Arrays.fill(minWeight, Integer.MAX_VALUE);
-        priorityQueue.add(new Vertex(start, 0));
-        minWeight[start] = 0;
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+        pq.add(new Vertex(start, 0));
 
-
-        while (!priorityQueue.isEmpty()) {
-            Vertex poll = priorityQueue.poll();
+        while (!pq.isEmpty()) {
+            Vertex poll = pq.poll();
             int pollNum = poll.num;
             int pollWeight = poll.weight;
-
 
             if (visited[pollNum]) continue;
             visited[pollNum] = true;
 
-            if (allvisited()) {
-                return minWeight[pollNum];
-            }
+            total += pollWeight;
 
-
-            for (Vertex endVertex: graph.get(pollNum)) {
-                // 끝점 정보들
-                int endNum = endVertex.num;
-                int endWeight = endVertex.weight;
-
-                // 끝점 최단거리가 endWeight 보다 크면
-                if (minWeight[endNum] > pollWeight + endWeight && !visited[endNum]) {
-                    minWeight[endNum] = pollWeight + endWeight;
-                    priorityQueue.add(new Vertex(endNum, pollWeight + endWeight));
-                }
+            for (Vertex endVertex : graph.get(pollNum)) {
+                if (visited[endVertex.num]) continue;
+                pq.add(endVertex);
             }
         }
-
-
-        return 0;
     }
-
-    private boolean allvisited() {
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    
 }

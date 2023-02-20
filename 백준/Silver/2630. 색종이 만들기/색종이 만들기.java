323454bf7b90a23699aa3b593030b50269fbb6d2
이정인 <dlwjddls0130@naver.com>
2023-02-20 12:17:27 +0900

    [Silver II] Title: 색종이 만들기, Time: 108 ms, Memory: 13276 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,67 +1,66 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int[][] colorPaper;
-    private static int blueCount = 0;
-    private static int whiteCount = 0;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        colorPaper = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                colorPaper[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        //------------------------------------------------------------
-
-        same(0,0,N);
-
-        bw.write(String.valueOf(whiteCount) + "\n" + String.valueOf(blueCount));
-
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-
-    private static void same(int row, int column, int N) {
-        boolean isSame = dividePaper2(row, column, N);
-        if (isSame) {
-            if (colorPaper[row][column] == 1) {
-                blueCount++;
-            } else {
-                whiteCount++;
-            }
-        }
-        else {
-            int newSize = N / 2;
-            same(row, column, newSize);
-            same(row, column + newSize, newSize);
-            same(row + newSize, column, newSize);
-            same(row + newSize, column + newSize, newSize);
-        }
-    }
-
-    private static boolean dividePaper2(int row, int column, int size) {
-
-        int standardColor = colorPaper[row][column]; // 기준
-
-        for (int i = row; i < row + size; i++) {
-            for (int j = column; j < column + size; j++) {
-                if (colorPaper[i][j] == standardColor) {
-                    continue;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	private static int N;
+	private static int[][] paper;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		N = Integer.parseInt(br.readLine());
+		
+		paper = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				paper[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		// 색종이 완성
+		
+		color(N, 0, 0);
+		
+		System.out.println(white);
+		System.out.println(blue);
+		  
+		
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	private static int blue = 0;
+	private static int white = 0;
+	private static void color(int n, int row, int column) {
+		boolean isOk = true;
+		int color = paper[row][column];
+		int size = n / 2;
+		for (int i = row; i < row + n; i++) {
+			for (int j = column; j < column + n; j++) {
+				if (paper[row][column] == paper[i][j]) {
+					continue;
+				}
+				isOk = false;
+				break;
+			}
+		}
+		if (!isOk) {
+			color(size, row, column);
+			color(size, row, column + size);
+			color(size, row + size, column);
+			color(size, row + size, column + size);
+		} else {
+			if (paper[row][column] == 1) {
+				blue++;
+			} else {
+				white++;
+			}
+		}
+	}
 }

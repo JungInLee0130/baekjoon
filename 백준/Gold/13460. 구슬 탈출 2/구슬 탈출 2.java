1e158a5d1b375862f76d06fe078dff578077f44b
이정인 <dlwjddls0130@naver.com>
2023-05-11 22:58:13 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 76 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -6,152 +6,146 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static char[][] map;
-	static boolean[][][][] visited;
-	static int holeX, holeY;
-	static Marble blue, red;
-	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1}; //0, 1, 2, 3 (상, 우, 하, 좌) - 시계 방향 
+    static int N, M;
+    static char[][] map;
+    static boolean[][][][] visited; // map, red, blue
+    static Hole hole; // 구명 좌표
+    static Marble red, blue;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1}; // 상우하좌
+    static class Marble{
+        int rx;
+        int ry;
+        int bx;
+        int by;
+        int cnt;
 
-		N = Integer.parseInt(st.nextToken());		
-		M = Integer.parseInt(st.nextToken());		
-		
-		map = new char[N][M];
-		visited = new boolean[N][M][N][M];
+        public Marble(int rx, int ry, int bx, int by, int cnt) {
+            this.rx = rx;
+            this.ry = ry;
+            this.bx = bx;
+            this.by = by;
+            this.cnt = cnt;
+        }
+    }
+    static class Hole{
+        int x;
+        int y;
 
-		// 구슬 map 구성 
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j);
-				
-				if(map[i][j] == 'O') {
-					holeX = i;
-					holeY = j;
-				} else if(map[i][j] == 'B') {
-					blue = new Marble(0, 0, i, j, 0);
-				} else if(map[i][j] == 'R') {
-					red = new Marble(i, j, 0, 0, 0);
-				}
-			}
-		}
+        public Hole(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        visited = new boolean[N][M][N][M];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j);
+                if (map[i][j] == 'B') {
+                    blue = new Marble(0, 0, i, j, 0);
+                }
+                if (map[i][j] == 'R') {
+                    red = new Marble(i, j, 0, 0, 0);
+                }
+                if (map[i][j] == 'O') {
+                    hole = new Hole(i, j);
+                }
+            }
+        }
 
-		System.out.println(bfs());
-		
-		br.close();
-	}
-	
-	public static int bfs() {
-		Queue<Marble> queue = new LinkedList<>();
-		queue.add(new Marble(red.rx, red.ry, blue.bx, blue.by, 1));
-		visited[red.rx][red.ry][blue.rx][blue.ry] = true;
-		
-		while(!queue.isEmpty()) {
-			Marble marble = queue.poll();
-			
-			int curRx = marble.rx;
-			int curRy = marble.ry;
-			int curBx = marble.bx;
-			int curBy = marble.by;
-			int curCnt = marble.cnt;
-		
-			if(curCnt > 10) { // 이동 횟수가 10 초과시 실패 
-				return -1;
-			}
-			
-			for(int i = 0; i < 4; i++) {
-				int newRx = curRx;
-				int newRy = curRy;
-				int newBx = curBx;
-				int newBy = curBy;
-				
-				boolean isRedHole = false;
-				boolean isBlueHole = false;
-				
-				// 빨간 구슬 이동 -> # 벽을 만날 때까지 이동 
-				while(map[newRx + dx[i]][newRy + dy[i]] != '#') { 
-					newRx += dx[i];
-					newRy += dy[i];
-					
-					// 이동 중 구멍을 만날 경우 
-					if(newRx == holeX && newRy == holeY) { 
-						isRedHole = true;
-						break;
-					}
-				}
-				
-				// 파란 구슬 이동 -> # 벽을 만날 때까지 이동 
-				while(map[newBx + dx[i]][newBy + dy[i]] != '#') { 
-					newBx += dx[i];
-					newBy += dy[i];
-					
-					// 이동 중 구멍을 만날 경우 
-					if(newBx == holeX && newBy == holeY) { 
-						isBlueHole = true;
-						break;
-					}
-				}
-				
-				if(isBlueHole) { // 파란 구슬이 구멍에 빠지면 무조건 실패 
-					continue; // 하지만 큐에 남은 다른 좌표도 봐야하니 다음으로 
-				}
-				
-				if(isRedHole && !isBlueHole) { // 빨간 구슬만 구멍에 빠지면 성공 
-					return curCnt;
-				}
-				
-				// 둘 다 구멍에 빠지지 않았는데 이동할 위치가 같은 경우 -> 위치 조정
-				if(newRx == newBx && newRy == newBy) {
-					if(i == 0) { // 위쪽으로 기울이기 
-						// 더 큰 x값을 가지는 구슬이 뒤로 감 
-						if(curRx > curBx) newRx -= dx[i]; 
-						else newBx -= dx[i];
-					} else if(i == 1) { // 오른쪽으로 기울이기 
-						// 더 작은 y값을 가지는 구슬이 뒤로 감 
-						if(curRy < curBy) newRy -= dy[i];
-						else newBy -= dy[i];	
-					} else if(i == 2) { // 아래쪽으로 기울이기 
-						// 더 작은 x값을 가지는 구슬이 뒤로 감 
-						if(curRx < curBx) newRx -= dx[i]; 
-						else newBx -= dx[i];
-					} else { // 왼쪽으로 기울이기 
-						// 더 큰 y값을 가지는 구슬이 뒤로 감 
-						if(curRy > curBy) newRy -= dy[i]; 
-						else newBy -= dy[i];	
-					}
-				}
-				
-				// 두 구슬이 이동할 위치가 처음 방문하는 곳인 경우만 이동 -> 큐에 추가 
-				if(!visited[newRx][newRy][newBx][newBy]) {
-					visited[newRx][newRy][newBx][newBy] = true;
-					queue.add(new Marble(newRx, newRy, newBx, newBy, curCnt+1));
-				}
-			}
-		}
-		
-		return -1;
-	}
+        System.out.println(bfs());
+    }
 
-}
+    private static int bfs() {
+        Queue<Marble> queue = new LinkedList<>();
+        queue.add(new Marble(red.rx, red.ry, blue.bx, blue.by, 1));
+        visited[red.rx][red.ry][blue.bx][blue.by] = true;
 
-class Marble {
-	int rx;
-	int ry;
-	int bx;
-	int by;
-	int cnt;
-	
-	Marble(int rx, int ry, int bx, int by, int cnt) {
-		this.rx = rx;
-		this.ry = ry;
-		this.bx = bx;
-		this.by = by;
-		this.cnt = cnt;
-	}
+        while (!queue.isEmpty()) {
+            Marble poll = queue.poll();
+
+            int curRr = poll.rx;
+            int curRc = poll.ry;
+            int curBr = poll.bx;
+            int curBc = poll.by;
+            int curCnt = poll.cnt;
+
+            if (curCnt > 10) {
+                return -1;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int newRr = curRr;
+                int newRc = curRc;
+                int newBr = curBr;
+                int newBc = curBc;
+
+                boolean isRedHole = false;
+                boolean isBlueHole = false;
+
+                // 한꺼번에 이동
+                // 빨간 구슬 이동
+                while (map[newRr + dr[d]][newRc + dc[d]] != '#'){
+                    newRr += dr[d];
+                    newRc += dc[d];
+
+                    if (newRr == hole.x && newRc == hole.y){
+                        isRedHole = true;
+                        break;
+                    }
+                }
+
+                // 파란 구슬 이동
+                while (map[newBr + dr[d]][newBc + dc[d]] != '#') {
+                    newBr += dr[d];
+                    newBc += dc[d];
+
+                    if (newBr == hole.x && newBc == hole.y) {
+                        isBlueHole = true;
+                        break;
+                    }
+                }
+
+                // 파란공 빠짐 : 걸러냄
+                if (isBlueHole) continue;
+                // 파란공 안빠지고, 빨간공이 빠짐. -> 최소횟수 리턴
+                if (isRedHole) return curCnt;
+
+                // 둘다 구멍에 빠지지않고 : 위치가 같은경우 ->
+                if (newRr == newBr && newRc == newBc) {
+                    if (d == 0) { // 위쪽으로 기울이기 : 더 큰쪽이 뒤
+                        if (curRr > curBr) newRr -= dr[d];
+                        else newBr -= dr[d];
+                    } else if (d == 1) { // 오른쪽으로 기울이기 : 더 작은쪽이 뒤
+                        if (curRc < curBc) newRc -= dc[d];
+                        else newBc -= dc[d];
+                    } else if (d == 2) { // 아래쪽 : 더 작은게 뒤
+                        if (curRr < curBr) newRr -= dr[d];
+                        else newBr -= dr[d]; // 틀이 짜여진뒤에는 오타 싸움.
+                    } else{ // 왼쪽 : 더 큰게 뒤
+                        if (curRc > curBc) newRc -= dc[d];
+                        else newBc -= dc[d];
+                    }
+                }
+
+                // 이미 방문한 경우
+                if (visited[newRr][newRc][newBr][newBc]) {
+                    continue;
+                }
+
+                // 새로운 경로
+                visited[newRr][newRc][newBr][newBc] = true;
+                queue.add(new Marble(newRr, newRc, newBr, newBc, curCnt + 1));
+            }
+        }
+        return -1;
+    }
 }

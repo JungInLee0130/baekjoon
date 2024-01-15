417d190aa70310b5a40a7c539bee5485a244ef28
이정인 <dlwjddls0130@naver.com>
2024-01-15 22:43:55 +0900

    [Silver I] Title: 1로 만들기 2, Time: 112 ms, Memory: 19992 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,40 +1,52 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
- 
-        ////////////////////////입력 끝
- 
-        int dp[] = new int[N + 1]; //최소 횟수 저장
-        int before[] = new int[N + 1]; //경로 저장
- 
-        String str = "";
- 
-        dp[1] = 0;
- 
-        for(int i = 2; i <= N; i++) {
+    static int N;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+
+        int[] before = new int[N + 1]; // 1 ~ N
+        dp = new int[N + 1];
+
+        for (int i = 2; i <= N; i++) {
             dp[i] = dp[i - 1] + 1;
             before[i] = i - 1;
- 
+            
             if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
                 dp[i] = dp[i / 3] + 1;
                 before[i] = i / 3;
-            }
+            } 
             if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
-                dp[i] = dp[i / 2] + 1;
                 before[i] = i / 2;
+                dp[i] = dp[i / 2] + 1;
             }
         }
+
         System.out.println(dp[N]);
- 
-        while(N > 0){
-            str += N + " ";
-            N = before[N];
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        int s = N;
+
+        while (true) {
+            arrayList.add(s);
+
+            if (s == 1) break;
+            s = before[s];
         }
- 
-        System.out.print(str);
- 
+
+        for (Integer e : arrayList) {
+            System.out.print(e + " ");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }

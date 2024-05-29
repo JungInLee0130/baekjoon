6fcdbad247cb16a38d24481b269ed6f825d085cb
이정인 <dlwjddls0130@naver.com>
2024-05-29 22:37:34 +0900

    [Silver II] Title: 쇠막대기, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -8,19 +8,6 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    static int M, N;
-    static class Stick{
-        int start;
-        int end;
-
-        int razerNum;
-
-        public Stick(int start, int end, int razerNum) {
-            this.start = start;
-            this.end = end;
-            this.razerNum = razerNum;
-        }
-    }
     static String str;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,59 +15,27 @@ public class Main {
 
         str = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
-
-        Stack<Integer> indexStack = new Stack<>();
-
-        ArrayList<Stick> stickList = new ArrayList<>();
-
-        sb.append(str.charAt(0));
-        for (int i = 1; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            if (str.charAt(i - 1) == '(' && str.charAt(i) == ')') {
-                sb.append('.');
+        int open = 0; // 괄호의 닫힘 여부
+        int ans = 0; // 통나무 개수
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                open++;
             }
-            sb.append(c);
-        }
+            else {
+                open--; // 닫음
 
-        ArrayList<Integer> razerPoint = new ArrayList<>();
-
-        for (int i = 0; i < sb.length() - 1; i++) {
-            char c = sb.charAt(i);
-
-            if (sb.charAt(i) == '('
-                    && sb.charAt(i + 1) == '.') {
-                razerPoint.add(i + 1);
-                i = i + 2;
-            }
-            else if (sb.charAt(i) == '(') {
-                indexStack.push(i);
-            } else if (sb.charAt(i) == ')') {
-                stickList.add(new Stick(indexStack.pop(), i, 0));
-            }
-        }
-
-        if (sb.charAt(sb.length() - 2) == ')' && sb.charAt(sb.length() - 1) == ')') {
-            stickList.add(new Stick(indexStack.pop(), sb.length() - 1, 0));
-        }
-
-        // 레이저는 .로 다표시됨.
-        //System.out.println(sb);
-
-        int res = 0;
-        for (Stick s:stickList) {
-            //System.out.println(s.start +" " + s.end + " ");
-            for (Integer e: razerPoint) {
-                if (s.start <= e && e <= s.end) {
-                    s.razerNum++;
+                if (i > 0) {
+                    if (str.charAt(i - 1) == '(') { // 레이저인 경우
+                        ans += open;
+                    }
+                    else{ // 나머지 막대인경우
+                        ans++;
+                    }
                 }
             }
-            //System.out.println(s.razerNum + 1);
-            res += s.razerNum + 1;
         }
 
-        System.out.println(res);
+        System.out.println(ans);
 
         bw.flush();
         bw.close();

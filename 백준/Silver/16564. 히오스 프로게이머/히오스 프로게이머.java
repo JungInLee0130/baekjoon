4a8880f3f6acb5bc2c68b4cdf2fd4e4f1c3d943f
이정인 <dlwjddls0130@naver.com>
2024-08-12 21:51:06 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 308 ms, Memory: 27512 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
@@ -2,40 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static long K;
+    static long[] levels;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int n = Integer.parseInt(st.nextToken());
-        long k = Long.parseLong(st.nextToken());
-        long[] level = new long[n];
-        for(int i=0;i<n;i++){
-            level[i] = Long.parseLong(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Long.parseLong(st.nextToken());
+
+        levels = new long[N];
+        long left = 0;
+        long right = 0;
+        for (int i = 0; i < N; i++) {
+            levels[i] = Long.parseLong(br.readLine());
         }
 
-        Arrays.sort(level);
+        Arrays.sort(levels);
 
-        System.out.println(binarySearch(k,0,level[n-1]+k,level));
+        left = levels[0];
+        right = levels[N - 1] + K;
 
+        long answer = sol(left,right);
+
+        System.out.println(answer);
+
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
-    public static long binarySearch(long m,long min,long max,long[] arr){
-        long result = 0;
-        while(min<=max){
-            long mid = (min+max)/2;
-            long count = 0;
-            for(long i : arr){
-                if(mid-i>0)
-                    count += mid-i;
-            }
-            if(count>m){
-                max = mid - 1;
+
+    private static long sol(long left, long right) {
+        return binarySearch(left, right);
+    }
+
+    private static long binarySearch(long left, long right) {
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (getK(mid) > K) {
+                right = mid - 1;
             }
             else{
-                result = mid;
-                min = mid + 1;
+                answer = mid;
+                left = mid + 1;
             }
         }
 
-        return result;
+        return answer;
+    }
+
+    private static long getK(long mid) {
+        long value = 0;
+        for (int i = 0; i < N; i++) {
+            if (mid > levels[i]) {
+                value += mid - levels[i];
+            }
+        }
+        return value;
     }
 }

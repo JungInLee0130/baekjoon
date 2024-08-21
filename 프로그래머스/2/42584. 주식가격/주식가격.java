c6534225257c5e762b0f904453d43d0955c592b3
이정인 <dlwjddls0130@naver.com>
2024-08-21 14:12:42 +0900

    [level 2] Title: 주식가격, Time: 34.11 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,32 @@
+import java.util.*;
+
+// 히스토그램이랑 유사..
 class Solution {
     static int N;
     public int[] solution(int[] prices) {
         int[] answer = {};
         
+        // N이 1억이라면?
         N = prices.length;
         
         answer = new int[N];
-        for (int i = 0; i < N; i++){
-            for (int j = i + 1; j < N; j++){
-                if (prices[i] <= prices[j]){
-                    answer[i]++;
-                }
-                else{
-                    answer[i]++;
-                    break;
-                }
+        // 인덱스 저장
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(0);
+        
+        for (int i = 1; i < N; i++){
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                int preIdx = stack.pop();
+                answer[preIdx] = i - preIdx; // 개수 저장
             }
+            stack.push(i);
         }
         
+        while (!stack.isEmpty()){
+            int idx = stack.pop();
+            answer[idx] = N - 1 - idx;
+        }
         
         return answer;
     }

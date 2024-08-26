f8aa9dd45e78a70bdfab40366d361a7d567f397a
이정인 <dlwjddls0130@naver.com>
2024-08-26 17:08:31 +0900

    [Silver III] Title: 수강신청, Time: 1360 ms, Memory: 74668 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -12,28 +12,17 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> map = new HashMap<>();
+        LinkedHashSet<String> linkedSet = new LinkedHashSet<>();
         for (int i = 0; i < L; i++) {
             String str = br.readLine();
             // 계속 갱신됨.
-            map.put(str, i);
+            if (linkedSet.contains(str)) {
+                linkedSet.remove(str);
+            }
+            linkedSet.add(str);
         }
 
-        // value를 오름차순으로 정렬
-        List<String> keySet = new ArrayList<>(map.keySet());
-
-        keySet.sort(new Comparator<String>() {
-            @Override
-            public int compare(String ia, String ib) {
-                return map.get(ia).compareTo(map.get(ib));
-            }
-        });
-
-        /*for (Integer e:keySet) {
-            System.out.println(e + " : " + map.get(e));
-        }*/
-
-        for (String s:keySet) {
+        for (String s:linkedSet) {
             K--;
             System.out.println(s);
             if (K == 0) {

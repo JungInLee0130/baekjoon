d6f1fde8fb966569a290c29a3616484f3b9e3e28
이정인 <dlwjddls0130@naver.com>
2024-08-27 20:49:08 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 148 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -3,15 +3,6 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static class Egg{
-        int d;
-        int w;
-
-        public Egg(int d, int w) {
-            this.d = d;
-            this.w = w;
-        }
-    }
     static int[][] eggs;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,37 +26,34 @@ public class Main {
 
     private static int max;
     private static void permu(int idx, int cnt) {
+        // 오른쪽 끝에 도달
         if (idx == N) {
             // 인덱스 모집 완료
             max = Math.max(max, cnt);
             return;
         }
 
+        // idx 계란이 내구도를 다하거나, 모든 계란이 깨짐
         if (eggs[idx][0] <= 0 || cnt == N - 1){
-            permu(idx + 1, cnt);
+            permu(idx + 1, cnt); // 계속 다음계란으로 옮김 : 최대값을 도출해야하기때문
             return;
         }
-
-        // nC2 해서 계란 부딫히기
-        int ncnt = cnt;
+        
         for (int i = 0; i < N; i++) {
-            // 자기 자신
-            if (idx == i) continue;
-            // 내구도 0
-            if (eggs[i][0] <= 0) continue;
+            // 자기 자신 or 치려는 계란의 내구도가 0
+            if (idx == i || eggs[i][0] <= 0) continue;
+            
+            // 새로운 내구도로 한다.
+            int ncnt = cnt;
+            // 두계란 부딫힘 : 내구도 깎임
+            ncnt += eggFight(idx, i); // idx와 i랑 fight
 
-            cnt += eggFight(idx, i); // idx와 i랑 fight
+            // 부딫힌상태를 가지고 계속, 다음 계란 : 한번했으니까 다음
+            permu(idx + 1, ncnt);
 
-            // 다음 계란 : 한번했으니까 다음
-            permu(idx + 1, cnt);
-
+            // 다음 번을 알아보기위해 내구도를 복구한다. cnt는 자동으로 복구됨. 이전에 ncnt를 넣었기때문에
             eggRecover(idx, i);
-            cnt = ncnt;
         }
-    }
-
-    private static void recover() {
-
     }
 
     private static void eggRecover(int a, int b) {
@@ -73,7 +61,6 @@ public class Main {
         eggs[b][0] = eggs[b][0] + eggs[a][1];
     }
 
-    private static boolean[] isBroken;
     private static int eggFight(int a, int b) {
         int cnt = 0;
 

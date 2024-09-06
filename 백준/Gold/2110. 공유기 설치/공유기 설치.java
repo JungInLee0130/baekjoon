a9f5b509b30005ab8baf367eae1c2a4b8efd5388
이정인 <dlwjddls0130@naver.com>
2024-09-06 12:00:06 +0900

    [Gold IV] Title: 공유기 설치, Time: 228 ms, Memory: 28520 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,61 +1,65 @@
-import java.awt.*;
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     static int N, C;
+    static int[] routers;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // N <= 20만
-        C = Integer.parseInt(st.nextToken()); // 2개이상 N 이하
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        x = new int[N]; // x[i] <= 10억
+        routers = new int[N];
         for (int i = 0; i < N; i++) {
-            x[i] = Integer.parseInt(br.readLine());
+            routers[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(x);
+        Arrays.sort(routers);
 
-        bs();
+        int dis = binarySearch(1, routers[N - 1] - routers[0] + 1);
+
+        System.out.println(dis);
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void bs() {
-        int result = 0;
 
-        int low = 1; // 최소간격
-        int high = x[N - 1] - x[0]; // 최대간격
+    private static int binarySearch(int left, int right) {
+        int answer = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-        while (low <= high) {
-            int cnt = 1; // 맨 처음에 둠
-            int pos = 0;
+            int count = getCount(mid);
 
-            int mid = (low + high) / 2;
-
-            for (int i = 1; i < N; i++) {
-                if (x[i] - x[pos] >= mid) {
-                    cnt++;
-                    pos = i;
-                }
+            if (count >= C) { // 개수 더 많음 ->
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
+        }
+        return answer;
+    }
 
-            if (cnt >= C) { // 공유기 이상이거나 같으면
-                result = mid;
-                low = mid + 1; // 간격늘리기
+
+    private static int getCount(int mid) {
+        int position = 0;
+        int count = 1;
+
+        for (int i = 1; i < N; i++) {
+            int sub = routers[i] - routers[position] + 1;
+            if (sub > mid) {
+                position = i;
+                count++;
             }
-            else{ // 적으면
-                high = mid - 1; // 간격좁히기
-            }
-            //System.out.println(low+ "," + high);
         }
 
-        System.out.println(high);
+        return count;
     }
-
-    static int[] x;
-    static int[] spot;
-    static int maxLen = 0;
 }

5749b9af38e69b3538c3ebf495d7d39aa550b463
이정인 <dlwjddls0130@naver.com>
2024-09-19 20:41:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 416.23 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,52 +1,46 @@
 import java.util.*;
 class Solution {
-    static PriorityQueue<Integer> pqMin, pqMax;
+    // deque를 LinkedList로 구현
+    // 이러면 정렬가능
+    static LinkedList<Integer> deque; 
     static int N;
     public int[] solution(String[] operations) {
         int[] answer = {0,0};
         
         N = operations.length;
         
-        pqMin = new PriorityQueue<>();
-        pqMax = new PriorityQueue<>(Collections.reverseOrder());
+        deque = new LinkedList<>();
         
         for (int i = 0; i < N; i++){
             String[] sp = operations[i].split("\\s"); // 공백
+            String command = sp[0];
+            int num = Integer.parseInt(sp[1]);
             
-            switch(sp[0]){
+            switch(command){
                 case "I" :
-                    int element = Integer.parseInt(sp[1]);
-                    pqMin.add(element);
-                    pqMax.add(element);
+                    deque.add(num);
+                    Collections.sort(deque);
                     break;
                 case "D" :
-                    if (pqMin.isEmpty() && pqMax.isEmpty()) break;
+                    // 빈큐 : 무시
+                    if (deque.isEmpty()) break;
                     // 최솟값
-                    else if (sp[1].equals("-1")){
-                        int poll = pqMin.poll();
-                        pqMax.remove(Integer.valueOf(poll));
+                    else if (num == -1){
+                        deque.pollFirst();
                     }
+                    // 최댓값
                     else{
-                        int poll = pqMax.poll();
-                        pqMin.remove(Integer.valueOf(poll));
+                        deque.pollLast();
                     }
+                    
                     break;
             }
         }
         
-        if (!pqMin.isEmpty() && !pqMax.isEmpty()){
-            if (pqMin.size() == 1){
-                int poll = pqMin.poll();
-                answer[0] = poll;
-                answer[1] = poll;
-            }
-            else{
-                int min = pqMin.poll();
-                int max = pqMax.poll();
-                
-                answer[0] = max;
-                answer[1] = min;
-            }
+        // 비어있으면 그냥 {0,0} 리턴
+        if (!deque.isEmpty()){
+            answer[0] = deque.getLast();
+            answer[1] = deque.getFirst();
         }
         
         return answer;

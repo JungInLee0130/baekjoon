939721f0eb021a30c39bac7d050cbeee54e4b69e
이정인 <dlwjddls0130@naver.com>
2024-10-24 02:53:49 +0900

    [level 3] Title: 거스름돈, Time: 11.22 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,25 +1,17 @@
 class Solution {
     static int cnt;
     public int solution(int n, int[] money) {
-        //%1_000_000_007;
-        
-        long[] dp = new long[n + 1]; // 0 ~ n
-        
-        // 초기(화폐 한개 사용) : (경우의 수)
-        for (int i = 0; i <= n; i++){
-            if (i % money[0] == 0){
-                dp[i] = 1;
-            }
-        }
-        
-        for (int i = 1; i < money.length; i++){
-            for (int j = money[i]; j <= n; j++){
-                dp[j] = dp[j] + dp[j - money[i]];
-            }
-        }
-        
-        cnt = (int)(dp[n] % 1_000_000_007);
-        
+        combi(n, money, 0, 0);
         return cnt;
+    }
+    private static void combi(int total, int[] money, int idx, int curPrice){
+        if (curPrice > total) return;
+        if (curPrice == total){
+            cnt++;
+            return;
+        }
+        for (int i = idx; i < money.length; i++){
+            combi(total, money, i, curPrice + money[i]);
+        }
     }
 }

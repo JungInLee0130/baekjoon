5bd587dd1a0e2c140a6676bf7c7b788656058c3e
이정인 <dlwjddls0130@naver.com>
2024-11-09 08:50:37 +0900

    [level 3] Title: 불량 사용자, Time: 145.59 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    String[] userIds;
+    String[] bannedIds;
+    boolean[] visited;
+    
+    HashSet<HashSet<String>> result = new HashSet<>();
+    
+    public int solution(String[] user_id, String[] banned_id) {
+        userIds= user_id;
+        bannedIds = banned_id;
+        visited = new boolean[userIds.length];
+        
+        dfs(new HashSet<>(), 0);
+    
+        return result.size();
+    }
+    
+    private void dfs(HashSet<String> set, int depth){
+        if (depth == bannedIds.length){
+            result.add(set);
+            return;
+        }
+        for (int i = 0; i < userIds.length; i++){
+            if (set.contains(userIds[i])) continue;
+            
+            if (check(userIds[i], bannedIds[depth]))
+            {
+                set.add(userIds[i]);
+                dfs(new HashSet<>(set), depth + 1);
+                set.remove(userIds[i]);
+            }
+        }
+    }
+    
+    private boolean check(String userId, String bannedId){
+        if (userId.length() != bannedId.length()) return false;
+        
+        for (int i = 0; i < userId.length(); i++){
+            if (bannedId.charAt(i) == '*') continue;
+            if (userId.charAt(i) != bannedId.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

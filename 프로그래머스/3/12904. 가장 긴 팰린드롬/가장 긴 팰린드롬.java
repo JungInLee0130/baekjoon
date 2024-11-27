e13fea670efd1aaa6e5748a9e820c6cc6d89ac10
이정인 <dlwjddls0130@naver.com>
2024-11-27 18:41:01 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 26.03 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Solution
+{
+    static int N;
+    public int solution(String s)
+    {
+        N = s.length();
+        if (N == 1) return 1;
+        
+        if (N == 2){
+            if (s.charAt(0) == s.charAt(1)){
+                return 2;
+            }
+            return 1;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < N; i++){
+            sb.append(s.charAt(i));
+        }
+        
+        
+        int max = 1;
+        
+        for (int i = 1; i <= N - 2; i++){
+            int leftIdx = i - 1;
+            int rightIdx = i + 1;
+
+            int len = 1;
+
+            while (true){
+                if (sb.charAt(leftIdx) == sb.charAt(rightIdx)){
+                    leftIdx--;
+                    rightIdx++;
+                    len += 2;
+                    if (leftIdx < 0 || rightIdx > N - 1){
+                        break;
+                    }
+                }
+                else{
+                    break;
+                }
+            }
+            if (len > 1){
+                max = Math.max(len, max);
+            }
+        }
+
+        for (int i = 1; i <= N - 2; i++){
+            int leftIdx = i;
+            int rightIdx = i + 1;
+            int len = 0;
+
+
+            while (true){
+                if (sb.charAt(leftIdx) == sb.charAt(rightIdx)){
+                    len += 2;
+                    leftIdx--;
+                    rightIdx++;
+                    if (leftIdx < 0 || rightIdx > N - 1){
+                        break;
+                    }
+                }
+                else{
+                    break;
+                }
+            }
+            if (len > 1){
+                max = Math.max(max, len);
+            }
+        }
+        
+        
+        if (sb.charAt(0) == sb.charAt(1)){
+            max = Math.max(max, 2);
+        }
+        
+        if (sb.charAt(N - 2) == sb.charAt(N - 1)){
+            max = Math.max(max, 2);
+        }
+        
+        return max;
+    }
+}

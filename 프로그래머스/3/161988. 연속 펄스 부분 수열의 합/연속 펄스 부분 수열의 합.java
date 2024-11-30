698f2ab5eecec3524114b3e6cd43a9aadc733428
이정인 <dlwjddls0130@naver.com>
2024-11-30 14:42:26 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 23.28 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -29,18 +29,20 @@ class Solution {
         sum1[0] = pulseUpSequence[0];
         sum2[0] = pulseDownSequence[0];
         
+        // 처음도 초기값을 잡아야한다... long.MIN_VALUE 쓰면안됨.
         long max = Math.max(sum1[0], sum2[0]);
         
         for (int i = 1; i < N; i++){
-            // 누적합 vs 이전것만
-            sum1[i] = Math.max(sum1[i - 1],
-                               pulseUpSequence[i - 1]) + pulseUpSequence[i];
-            //sum1[i] = Math.max(sum1[i], pulseUpSequence[i]);
+            // 1. 누적합 vs 이전것만
+            // i == 1 부터 시작하기때문에
+            // 1에서는 이전원소와 자신의 합과 자기자신을 비교하게된다.
+            // 따라서 누적합 vs 이전것만 -> 누적합 + 이전것만을 합치고 vs 자기자신 으로
+            // 합칠수있다.
+            sum1[i] = Math.max(sum1[i - 1] + pulseUpSequence[i],
+                               pulseUpSequence[i]);
             
-            sum2[i] = Math.max(sum2[i - 1],
-                              pulseDownSequence[i - 1])  + pulseDownSequence[i];
-            
-            //sum2[i] = Math.max(sum2[i], pulseDownSequence[i]);
+            sum2[i] = Math.max(sum2[i - 1] + pulseDownSequence[i],
+                               pulseDownSequence[i]);
             
             max = Math.max(sum1[i], max);
             max = Math.max(sum2[i], max);

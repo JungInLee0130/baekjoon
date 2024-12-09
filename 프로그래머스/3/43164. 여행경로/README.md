6625242fd8be9514e165053e8b54f916b4c707fb
이정인 <dlwjddls0130@naver.com>
2024-12-09 17:11:25 +0900

    [level 3] Title: 여행경로, Time: 1.38 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 95.6 MB, 시간: 102.35 ms
+메모리: 77.5 MB, 시간: 1.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 16:34:03
+2024년 12월 09일 17:11:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,36 +1,64 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited;
-    List<String> allRoute;
-    
-    public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        int cnt = 0;
-        visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
+    static int N; // 지나는 공항의 개수 : tickets + 1
+    static class Ticket implements Comparable<Ticket>{
+        String start;
+        String end;
         
-        dfs("ICN", "ICN", tickets, cnt);
-        
-        Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
-        
-        return answer;
-    }
-    
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if (cnt == tickets.length){
-            allRoute.add(route);
-            return;
+        public Ticket(String start, String end){
+            this.start = start;
+            this.end = end;
         }
         
+        // 오름차순
+        @Override
+        public int compareTo(Ticket t){
+            if (start.equals(t.start)){
+                return end.compareTo(t.end);
+            }
+            return start.compareTo(t.start);
+        }
+    }
+    static Ticket[] Tickets;
+    public String[] solution(String[][] tickets) {
+        N = tickets.length + 1;
+        
+        // ICN 에서 시작해서 N개만큼 경로를 만들어야함.
+        Tickets = new Ticket[tickets.length];
+        
         for (int i = 0; i < tickets.length; i++){
-            if (start.equals(tickets[i][0]) && !visited[i]){
+            Tickets[i] = new Ticket(tickets[i][0], tickets[i][1]);
+        }
+        
+        Arrays.sort(Tickets);
+        
+        visited = new boolean[tickets.length];
+        
+        route = new String[N];
+        route[0] = "ICN";
+        
+        dfs("ICN", 1);
+        
+        return route;
+    }
+    static boolean[] visited;
+    static String[] route;
+    static boolean isFinished;
+    
+    static void dfs(String start, int count){
+        if (count >= N){
+            isFinished = true;
+            return;
+        }
+        for (int i = 0; i < N - 1; i++){ // ticket.length
+            if (isFinished || visited[i]) continue;
+            if (start.equals(Tickets[i].start)){
                 visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt + 1);
+                route[count] = Tickets[i].end;
+                dfs(Tickets[i].end, count + 1);
                 visited[i] = false;
             }
         }
     }
-   
 }

b1c7a8e39100a598356fd0c20cd6be324fcd5d85
이정인 <dlwjddls0130@naver.com>
2024-12-21 21:39:33 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.04 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -3,23 +3,19 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
         
-        // 현재 감당가능한 deliver, pickup 수
-        int d = 0;
-        int p = 0;
-        
+        int curD = 0;
+        int curP = 0;
         for (int i = n - 1; i >= 0; i--){
-            d -= deliveries[i];
-            p -= pickups[i];
+            curD -= deliveries[i];
+            curP -= pickups[i];
             
-            // 감당 불가능하면
-            while (d < 0 || p < 0){
-                d += cap;
-                p += cap;
+            while (curD < 0 || curP < 0){
+                curD += cap;
+                curP += cap;
                 
                 answer += (i + 1) * 2;
             }
         }
-        
         
         return answer;
     }

a6b3cea0d1cf450e841747b6287303759bc99cc6
이정인 <dlwjddls0130@naver.com>
2025-01-15 05:11:51 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 3184 ms, Memory: 18040 KB -BaekjoonHub

---
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static String str;
+    static int[] dp;
+    static boolean[][] palindrome;
+    static int len;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str = br.readLine();
+
+        len = str.length();
+
+        dp = new int[len];
+        palindrome = new boolean[len][len];
+
+        // dp[i] = 0 ~ i까지 펠린드롬의 최소개수
+        // palindrome[j][i] : j ~ i까지 문자열이 펠린드롬이면 true, 아니면 false
+        // dp[i] = (dp[j - 1]) + 1
+
+        checkPalindrome();
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        findAnswer();
+
+        System.out.println(dp[len - 1]);
+
+
+        br.close();
+    }
+
+    private static void findAnswer() {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++){
+                if (palindrome[j][i]) {
+                    if (j == 0) {
+                        dp[i] = Math.min(dp[i], 1);
+                    }
+                    else{
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void checkPalindrome() {
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int start = i;
+                int end = j;
+
+                boolean isPalindrome = true;
+                while (start <= end){
+                    if (str.charAt(start) != str.charAt(end)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    start++;
+                    end--;
+                }
+
+                if (isPalindrome) {
+                    palindrome[i][j] = true;
+                }
+                else{
+                    palindrome[i][j] = false;
+                }
+            }
+        }
+    }
+}

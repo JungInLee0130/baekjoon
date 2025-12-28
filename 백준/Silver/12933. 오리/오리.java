73c34fc9aecd9b7d4c80a69d636f82f3367bb413
이정인 <dlwjddls0130@naver.com>
2025-12-28 10:37:42 +0900

    [Silver II] Title: 오리, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -10,53 +10,39 @@ public class Main {
 
         String sound = br.readLine();
 
+        // 예외 1. 5로 나누어떨어지지않는경우
         if (sound.length() % 5 != 0) {
             System.out.println(-1);
             return;
         }
 
-        int count = 0;
+        int[] counts = new int[5];  // 오리 상태 배열
+        int totalDucks = 0;
 
-        boolean[] visited = new boolean[sound.length()];
-        char[] letters = {'q', 'u', 'a', 'c', 'k'};
-        int letterIdx = 0;
+        for (char c : sound.toCharArray()) {    // sound 배열 순회
+            int idx = "quack".indexOf(c);   // 찾지 못했을경우 -1 반환
 
-        for (int k = 0; k < sound.length(); k++) {  // 2500
-            if (sound.charAt(k) == 'q') {
-                letterIdx = 0;
-                boolean isDuck = false;
-                // q 시작점부터 끝까지
-                for (int j = k; j < sound.length(); j++) { // 2500
-                    if (sound.charAt(j) == letters[letterIdx]) {
-                        if (visited[j]) continue;
-                        visited[j] = true;
-                        letterIdx += 1;
-                        if (letterIdx >= 5) {
-                            isDuck = true;
-                            letterIdx = letterIdx % 5;
-                        }
-                    }
+            // 'q'
+            if (idx == 0) {
+                if (counts[4] > 0) counts[4]--; // 이미 완주한 duck이 있을경우 소비
+                else totalDucks++; // 완주한다는 가정하에
+                counts[0]++;
+            } else {
+                if (counts[idx - 1] == 0) { // 예외 2. 이전 duck이 없을경우.
+                    System.out.println(-1);
+                    return;
                 }
-                if (isDuck && letterIdx == 0) {
-                    count += 1;
-                    isDuck = false;
-                }
+                counts[idx - 1]--;  // 이전 duck - 1, 현재 duck + 1
+                counts[idx]++;
             }
         }
 
-        for (int i = 0; i < sound.length(); i++) {
-            if (!visited[i]) {
-                count = -1;
-                break;
-            }
-        }
-
-        if (count >= 1) {
-            System.out.println(count);
-        } else {
+        // 3. 합했을때 모든 이전 duck이 1이 있는 경우 
+        if (counts[0] + counts[1] + counts[2] + counts[3] > 0) {
             System.out.println(-1);
+        } else {
+            System.out.println(totalDucks == 0 ? -1 : totalDucks);
         }
-
 
 
         br.close();

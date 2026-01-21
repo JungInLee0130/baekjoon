dd84e94d00bcead2f2a95ce9c72a9460c3bab036
이정인 <dlwjddls0130@naver.com>
2026-01-21 17:01:49 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,50 +1,26 @@
 import java.util.*;
 
 class Solution {
-    private List<List<Integer>> graph;
+    private int count = 0;
     public int solution(int n, int[][] computers) {
-        graph = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        // 인접 행렬 -> 연결 그래프
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = i + 1; j < computers[i].length; j++) {
-                if (computers[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);    // 무방향그래프
-                }
-            }
-        }
-        
         isVisited = new boolean[n];
-        int count = 0;
         for (int i = 0; i < n; i++) {
             if (isVisited[i]) continue;
-            count += bfs(i, n);    
+            count += 1;
+            isVisited[i] = true;
+            dfs(i, n, computers);    
         }
         
         return count;
     }
     private boolean[] isVisited;
-    private int bfs(int start, int n) {
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start); // 시작점 : 0
-        int count = 1;  // 네트워크 : 1        
-        isVisited[start] = true;
-        
-        while (!que.isEmpty()) {
-            Integer poll = que.poll();
-            
-            for(Integer e : graph.get(poll)){
-                if (isVisited[e]) continue;
-                isVisited[e] = true;
-                que.add(e);
-            }
+    private void dfs(int start, int n, int[][] computers) {
+        for (int i = 0; i < n; i++){
+            if (isVisited[i]) continue;
+            if (start == i) continue;
+            if (computers[start][i] != 1) continue; 
+            isVisited[i] = true;
+            dfs(i, n, computers); // end : i + 1(end이자 다음 함수에서는 start)
         }
-        
-        return count;
     }
 }
